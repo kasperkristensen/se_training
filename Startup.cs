097ec6d94570a7cfade9955f560a5ada212f44ba
Frederik Avlund <favl@itu.dk>
2021-11-27 13:22:55 +0100

    s

--- Startup.cs
+++ Startup.cs
@@ -30,10 +30,9 @@ namespace se_training
             services.AddRazorPages();
             services.AddServerSideBlazor();
             
-               services.AddDbContext<SeContext>(options =>
+            services.AddDbContext<SeContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
